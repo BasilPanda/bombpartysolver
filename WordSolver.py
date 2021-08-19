64c14a1b72aa3dbea5f84480d4096ed88ee1cdbe
ewing.eng <eeng275@gmail.com>
2021-08-19 01:00:54 -0400

    added automatic keyboard input

--- WordSolver.py
+++ WordSolver.py
@@ -5,7 +5,7 @@ import pytesseract
 from PIL import ImageGrab
 import keyboard
 
-filepath_words = "..\\dict\\dict.txt"
+filepath_words = "dict\\dict.txt"
 input_coords = [736, 1371, 1455, 1414]
 letter_coords = [2059, 197, 2181, 1339]
 bomb_coords = [1057, 759, 1123, 803]
@@ -23,48 +23,55 @@ def main():
         cap_letters = cv2.cvtColor(cap_letters, cv2.COLOR_BGR2GRAY)
         cap_bomb = np.array(ImageGrab.grab(bbox=bomb_coords))
         cap_bomb = cv2.cvtColor(cap_bomb, cv2.COLOR_BGR2GRAY)
-        # 20 is when it's my turn to input
-        # if cap_input_box[750 - input_coords[0], 1380 - input_coords[1]] <= 20:
+        # 20 is when it's current turn to input
+        if cap_input_box[750 - input_coords[0], 1380 - input_coords[1]] == 20:
             # run function
-        #    text = pytesseract.image_to_string(cap_bomb)
-        #    print(text)
-        #    time.sleep(1)
-        thresh = 255 - cv2.threshold(cap_bomb, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+            text = pytesseract.image_to_string(cap_bomb)
+            #print(text)
 
-        # thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
-        data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 7 tessedit_char_whitelist'
-                                                                      '=abcdefghijklmnopqrstuvwxyz')
-        letters = data.rstrip().lower()
-        print(letters)
-        if letters.isalpha():
-            if 20 >= cap_input_box[750 - input_coords[0], 1380 - input_coords[1]]:
-                print(f"Int: {cap_input_box[750 - input_coords[0], 1380 - input_coords[1]]}")
-                # temp = cap_input_box[750 - input_coords[0], 1380 - input_coords[1]]
-            valid = getValid(letters)
-            best = findLongestUnique(valid, used)
-            # print the best word
-            print(best)
-        time.sleep(0.25)
-        # text = pytesseract.image_to_string(cap_letters)
-        # print(text)
+            thresh = 255 - cv2.threshold(cap_bomb, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-        cv2.imshow("thresh", thresh)
+            # thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
+            data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 7 -c tessedit_char_whitelist'
+                                                                          '=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
+            letters = data.rstrip().lower()
+            print(letters)
+            if letters.isalpha():
+                # if 20 >= cap_input_box[750 - input_coords[0], 1380 - input_coords[1]]:
+                    # print(f"Int: {cap_input_box[750 - input_coords[0], 1380 - input_coords[1]]}")
+                    # temp = cap_input_box[750 - input_coords[0], 1380 - input_coords[1]]
+                valid = getValid(letters)
+
+                # Get the best word possible that isn't already used
+                best = findLongestUnique(valid, used)
+
+                used.append(best)
+                print(best)
+                keyboard.write(best)
+                keyboard.press_and_release('enter')
+            time.sleep(0.25)
+            # text = pytesseract.image_to_string(cap_letters)
+            # print(text)
+
+            # Shows the capture of the bomb
+            # cv2.imshow("thresh", thresh)
+            # Wait
+            time.sleep(1)
 
         if cv2.waitKey(1) == 27:
             break
-    # 1440p monitor
-    # input box
-    # x736 y1371
-    # x1455 y1414
+        # 1440p monitor
+        # input box
+        # x736 y1371
+        # x1455 y1414
 
-    # letters on right
-    # x2059 y197
-    # x2181 y1339
+        # letters on right
+        # x2059 y197
+        # x2181 y1339
 
-    # words in middle
-    # x1057 y759
-    # x1123 y803
-
+        # words in middle
+        # x1057 y759
+        # x1123 y803
     cv2.destroyAllWindows()
 
 
@@ -96,4 +103,3 @@ def findLongestUnique(words, used):
 if __name__ == "__main__":
     loadWords()
     main()
-
